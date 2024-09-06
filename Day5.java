af70600df2b57816ae7c84ebd080aa02e12176b4
Mawan2024 <mwan6703@gmail.com>
2024-09-06 14:42:51 +0800

    Hari 5
    Merampikan komentar

---
+++ Day5.java
@@ -0,0 +1,18 @@
+
+package pkg100dayscoding;
+public class Days5 {
+    public static void main(String[] args) {
+    System.out.print("Nama mawan,prodi informatika");
+ System.out.println("\nkelas: B angkatan: 2024 ");
+  System.out.printf("saya berasal dari %s dan saya berumur %d tahun.", "mamasa", 17);
+    /* digunakan untuk mencetak text dengan format
+        %s = String untuk huruf
+        %d = untuk bilang bulat
+        %f = untuk float bilangan berkoma
+        %n = enter untuk baris baru
+        %b = boolean untuk true and false
+    */
+    
+    }
+    
+}
